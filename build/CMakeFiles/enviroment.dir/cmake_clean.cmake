97a6cb7c93ed1367ede8683316a93231a4338ae4
Maryia <mariababinskaya2804@gmail.com>
2023-11-13 15:29:09 +0300

    environment setup

---
+++ build/CMakeFiles/enviroment.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/enviroment.dir/boost.cpp.o"
+  "enviroment.pdb"
+  "enviroment"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/enviroment.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
